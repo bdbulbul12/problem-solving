0af8f32c3515e2e96fb1e2c1d7c3a35208de5e3c
BULBUL AHMED <bulbulahmed.cse@yahoo.com>
2018-06-06 01:33:32 +0600

    SumOfDigit File Added

---
+++ src/problem/SumOfDigit.java
@@ -0,0 +1,17 @@
+
+package problem;
+
+public class SumOfDigit {
+    
+    private  static int sumOfDigit(int num){
+        if(num==0){
+            return 0;
+        }
+        return num%10+sumOfDigit(num/10);
+    }
+    
+    public static void main(String[] args) {
+        int result = sumOfDigit(120);
+        System.out.println(result);
+    }
+}
